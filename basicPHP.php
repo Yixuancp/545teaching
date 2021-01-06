15d193dd3fa37e4b62e5af357ddce31386c6f115
Yixuancp <yixuan.liu@utexas.edu>
2021-01-05 22:33:43 -0500

    Create basicPHP.php

---
+++ basicPHP.php
@@ -0,0 +1,7 @@
+<?php
+print "<html>\n";
+print "<head><title>Basic PHP - Hello World</title></head>\n";
+print "<body>\n";
+print "Hello World!  This is my first PHP script\n";
+print "</body>\n</html>";
+?>
